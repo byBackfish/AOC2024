1596e13299b74821d0889340306ad06e182010df
Maik <maik@bybackfish.de>
2024-12-01 23:34:37 +0100

    cleanup

--- 01/src/main.rs
+++ 01/src/main.rs
@@ -2,7 +2,6 @@ mod p1;
 mod p2;
 
 fn main() {
-    // check the args, if its p1 then run p1, if its p2 then run p2
     let input = std::env::args().nth(1).expect("no input");
     if input == "p1" {
         p1::p1();

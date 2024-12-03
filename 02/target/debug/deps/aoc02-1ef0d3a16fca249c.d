9d0d0a7edaeb3461b081ecb2ad9fd523b51a4cbe
Maik <38255215+byBackfish@users.noreply.github.com>
2024-12-04 00:59:41 +0100

    Delete 02/target directory

--- 02/target/debug/deps/aoc02-1ef0d3a16fca249c.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Maik\Documents\AOC2024\02\target\debug\deps\libaoc02-1ef0d3a16fca249c.rmeta: src/main.rs src\util.rs src\p1.rs src\p2.rs
-
-C:\Users\Maik\Documents\AOC2024\02\target\debug\deps\aoc02-1ef0d3a16fca249c.d: src/main.rs src\util.rs src\p1.rs src\p2.rs
-
-src/main.rs:
-src\util.rs:
-src\p1.rs:
-src\p2.rs:
--- 02/target/debug/deps/aoc02-af11dc9d01cc264b.d
+++
@@ -1,8 +0,0 @@
-C:\Users\Maik\Documents\AOC2024\02\target\debug\deps\libaoc02-af11dc9d01cc264b.rmeta: src/main.rs src\util.rs src\p1.rs src\p2.rs
-
-C:\Users\Maik\Documents\AOC2024\02\target\debug\deps\aoc02-af11dc9d01cc264b.d: src/main.rs src\util.rs src\p1.rs src\p2.rs
-
-src/main.rs:
-src\util.rs:
-src\p1.rs:
-src\p2.rs:
